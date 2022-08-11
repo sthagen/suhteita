d340984396b083c55fd84e37d6845979d737738f
Stefan Hagen <stefan@hagen.link>
2022-08-11 21:20:58 +0200

    Added SMVP implementation

---
+++ suhteita/suhteita.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=expression-not-assigned,line-too-long
+"""API for relationships (Finnish: suhteita) maintained across distances as load test core."""
+# import os
+import sys
+from typing import List, Union
+
+
+def main(argv: Union[List[str], None] = None) -> int:
+    argv = sys.argv[1:] if argv is None else argv
+    return 0
