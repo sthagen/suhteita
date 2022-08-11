056573b36875e0faf2a164e5eae6ae9a30ae4ca0
Stefan Hagen <stefan@hagen.link>
2022-08-11 21:20:33 +0200

    Added main

---
+++ suhteita/__main__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=missing-docstring
+import sys
+
+from suhteita.cli import main
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv[1:]))  # pragma: no cover
