81a17fd99b461a687296079b4e157cd2ede7dd2f
Stefan Hagen <stefan@hagen.link>
2022-08-23 00:04:43 +0200

    Bumped impl version for release

--- suhteita/__init__.py
+++ suhteita/__init__.py
@@ -1,6 +1,6 @@
 # [[[fill git_describe()]]]
-__version__ = '2022.8.21+parent.dc77e154'
-# [[[end]]] (checksum: 3fc9a6f9a48c72a68249c46666d48295)
+__version__ = '2022.8.22+parent.cf7eead9'
+# [[[end]]] (checksum: 9945df48b52994eed750cc280f65a8cd)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
