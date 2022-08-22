626ded460f32d870075deed475da39db3c84f61e
Stefan Hagen <stefan@hagen.link>
2022-08-22 23:15:45 +0200

    Completed minimal coverage of CLI

--- test/test_cli.py
+++ test/test_cli.py
@@ -9,3 +9,12 @@ def test_main_nok_too_many_arguments():
     message = r'main\(\) takes from 0 to 1 positional arguments but 2 were given'
     with pytest.raises(TypeError, match=message):
         cli.main(1, 2)
+
+
+def test_main_ok_smvp(capsys):
+    with pytest.raises(SystemExit, match='0'):
+        cli.main(['-h'])
+    out, err = capsys.readouterr()
+    assert not err
+    assert 'show this help message and exit' in out
+    assert 'output folder path for recording' in out
