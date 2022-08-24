ac88ac0cda9ff54f793a318475f9acfc1f0a54bd
Stefan Hagen <stefan@hagen.link>
2022-08-24 23:02:05 +0200

    Added version adhoc to the implementation for logging (TODO)

--- suhteita/cli.py
+++ suhteita/cli.py
@@ -6,10 +6,11 @@ import sys
 from typing import List, Union
 
 import suhteita.suhteita as api
+from suhteita import __version__ as version
 
 
 # pylint: disable=expression-not-assigned
 def main(argv: Union[List[str], None] = None) -> int:
     """Delegate processing to functional module."""
     argv = sys.argv[1:] if argv is None else argv
-    return api.main(argv)
+    return api.main(argv, version=version)
