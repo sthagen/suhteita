159c26f6eb16882f376eafb0975300a42bd03888
Stefan Hagen <stefan@hagen.link>
2022-08-27 23:05:17 +0200

    Bumped impl version

--- suhteita/__init__.py
+++ suhteita/__init__.py
@@ -8,8 +8,8 @@ import uuid
 from typing import Tuple, no_type_check
 
 # [[[fill git_describe()]]]
-__version__ = '2022.8.24+parent.48ceea5e'
-# [[[end]]] (checksum: af6ef5820c89892f0cfbfa74f48a9dd8)
+__version__ = '2022.8.27+parent.9f7d45b8'
+# [[[end]]] (checksum: 6f197e33122cf49bec68f45a9c4b8337)
 __version_info__ = tuple(
     e if '-' not in e else e.split('-')[0] for part in __version__.split('+') for e in part.split('.') if e != 'parent'
 )
